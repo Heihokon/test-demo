e220f6a30e5a7e96eec6e87a26f88e016bac955b
Heihokon <neoshark22@gmail.com>
2022-08-19 14:36:25 +0100

    add @flagship.io/react-sdk package

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import logo from './logo.svg';
 import './App.css';
+import { FlagshipProvider } from '@flagship.io/react-sdk';
 
 function App() {
+
+  const params = new URL(window.location).searchParams;
+  const envId = params.get('envId');
+  const apiKey = params.get('apiKey')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <FlagshipProvider
+      envId={envId}
+      apiKey={apiKey}
+    >
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </FlagshipProvider>
+
   );
 }
 
