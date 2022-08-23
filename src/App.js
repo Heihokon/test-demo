fa5a48135713443855d10ac6a01138ed600abce8
Heihokon <neoshark22@gmail.com>
2022-08-23 14:31:49 +0100

    add route prefix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import { FlagshipProvider } from '@flagship.io/react-sdk';
 import { Container } from 'react-bootstrap'
 import Menu from './components/Menu';
 import { Routes, Route, useSearchParams } from "react-router-dom";
-import { routesList } from './constants/routesList';
+import { routesList, appPrefix } from './constants/routesList';
 import Index from './pages';
 
 
@@ -21,7 +21,7 @@ function App() {
       <Menu />
       <Container fluid="md">
         <Routes>
-          <Route path="/" element={<Index />} />
+          <Route path={appPrefix} element={<Index />} />
           {
             routesList.map((item, index) => (<Route key={index} path={item.path} element={<item.element />} />))
           }
--- src/constants/routesList.js
+++ src/constants/routesList.js
@@ -1,14 +1,15 @@
 import UseCase1 from "../pages/UseCase1";
 
+export const appPrefix = "test-demo"
 
 export const routesList = [
     {
-        path: "use-case-1",
+        path: appPrefix + "/use-case-1",
         element: UseCase1,
         name: "use case 1"
     },
     {
-        path: "use-case-2",
+        path: appPrefix + "/use-case-2",
         element: UseCase1,
         name: "use case 2"
     }
